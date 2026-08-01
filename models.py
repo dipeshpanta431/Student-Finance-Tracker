72b4118171f207a59d5c486b44aaaa1daea33e3a
Dipesh Panta <dipeshpanta431@gmail.com>
2026-08-01 11:56:30 +0545

    Add transaction model and user relationship

--- models.py
+++ models.py
@@ -16,8 +16,35 @@ class User(UserMixin, db.Model):
     password_hash = db.Column(db.String(255), nullable=False)
 
     created_at = db.Column(db.DateTime, default = datetime.utcnow)
-
+    
     def __repr__(self):
         return f"<User {self.email}>"
 
+class Transaction(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    amount = db.Column(db.Float, nullable=False)
+
+    transaction_type = db.Column(db.String(20), nullable=False)
+
+    category = db.Column(db.String(50), nullable=False)
+
+    payment_mode = db.Column(db.String(30), nullable=False)
+
+    date = db.Column(db.Date, nullable=False)
+
+    description = db.Column(db.String(255), nullable=True)
+
+    user_id = db.Column(
+        db.Integer,
+        db.ForeignKey("user.id"),
+        nullable=False
+    )
+
+transactions = db.relationship(
+    "Transaction",
+    backref="user",
+    lazy=True
+)
 
